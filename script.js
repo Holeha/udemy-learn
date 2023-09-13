f66288db369b65c82d000c2a7c1835c4ad34e66b
Daniil <holeha.daniil@gmail.com>
2023-09-13 21:03:06 +0200

    Создание функий

--- script.js
+++ script.js
@@ -23,7 +23,17 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -33,27 +43,47 @@ const personalMovieDB = {
     private: false
 };
 
-
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?',''),
-          b = prompt('На сколько оцените его?','');
-
-    if (a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50) {
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms () {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?',''),
+              b = prompt('На сколько оцените его?','');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50) {
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("You had watched a few films");
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-    console.log("You had watched enough films");
-} else if (personalMovieDB.count >= 30) {
-    console.log("You are cinemaddict");
-} else {
-    console.log('error');
+//rememberMyFilms ();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("You had watched a few films");
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        console.log("You had watched enough films");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("You are cinema addict");
+    } else {
+        console.log('error');
+    }
 }
 
-console.log(personalMovieDB);
+//detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres () {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Your favourite genre number ${i} `);
+    }
+}
+writeYourGenres ();
